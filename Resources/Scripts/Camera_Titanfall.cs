8d3927ef183d2defb5a96f429f68f6b7a23082dc
Joel Martin <joel.martin02@bigpond.com>
2024-04-08 21:10:26 +0930

    Need to fix Lerp in Wall_Left Camera movement

--- Resources/Scripts/Camera_Titanfall.cs
+++ Resources/Scripts/Camera_Titanfall.cs
@@ -6,9 +6,11 @@ public class Camera_Titanfall : MonoBehaviour
 {
     private GameObject Player;
     public float playerHeight_half = 1f, camSpeed = 360f;
+    private float targetRot;
     // Start is called before the first frame update
     void Start()
     {
+        targetRot = 0f;
         Cursor.lockState = CursorLockMode.Locked;
         Player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -16,9 +18,29 @@ public class Camera_Titanfall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) transform.rotation = Quaternion.identity;
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + playerHeight_half, Player.transform.position.z);
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * camSpeed * Time.deltaTime, Space.World);
         transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * camSpeed * Time.deltaTime, Space.Self);
+        if (Player.GetComponent<Player_Titanfall>().currentPlayerState == Player_Titanfall.PlayerState.Wall_Left) 
+        {
+            Debug.Log("Touching Wall on Left");
+            targetRot = -20f;
+            //transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, -0.001f);
+        }
+        else if (Player.GetComponent<Player_Titanfall>().currentPlayerState == Player_Titanfall.PlayerState.Wall_Right)
+        {
+            Debug.Log("Touching Wall on Right");
+            targetRot = 20f;
+            //transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, 0.001f);
+        }
+        else
+        {
+            targetRot = 0f;
+            //transform.localRotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, 0f);
+        }
+        transform.localRotation = Quaternion.Euler
+        (
+            transform.localEulerAngles.x,
+            transform.localEulerAngles.y, 
+            Mathf.Lerp(transform.localEulerAngles.z, targetRot, 0.1f)
+        );
     }
 }
--- Resources/Scripts/Player_Titanfall.cs
+++ Resources/Scripts/Player_Titanfall.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Titanfall : MonoBehaviour
 {
     private CharacterController controller;
-    public float moveSpeed = 1f, jumpSpeed = 1f, gravitySpeed = 9.81f;
-    private float gravityAccumulation = 0f;
+    public float moveSpeed = 1f, jumpSpeed = 1f, gravitySpeed = -9.81f, wallLaunchSpeed = 100f, momentumDamp = -10f;
+    private Vector3 momentum = Vector3.zero;
+    public enum PlayerState {Ground, Wall_Left, Wall_Right, Jumping};
+    public PlayerState currentPlayerState = PlayerState.Ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +19,72 @@ public class Player_Titanfall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // MOVEMENT
         float camY = Camera.main.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
         float camY_Cos = Mathf.Cos(camY);
         float camY_Sin = Mathf.Sin(camY);
+
         Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * camY_Cos + Input.GetAxis("Vertical") * camY_Sin, 0, 
             -(Input.GetAxis("Horizontal") * camY_Sin) + Input.GetAxis("Vertical") * camY_Cos);
-        if (velocity.magnitude > 1)
-            velocity.Normalize();
-        velocity *= moveSpeed;
-        if (!controller.isGrounded)
-            gravityAccumulation -= gravitySpeed * Time.deltaTime;
-        else gravityAccumulation = 0f;
-        velocity += Vector3.up * (Input.GetAxis("Jump") * jumpSpeed + gravityAccumulation);
+        if (velocity.magnitude > 1) velocity.Normalize();
+        velocity *= moveSpeed; // XZ Plane movement Complete
+        if (currentPlayerState == PlayerState.Jumping)
+            momentum.y += gravitySpeed * Time.deltaTime;
+        else if (currentPlayerState == PlayerState.Wall_Left || currentPlayerState == PlayerState.Wall_Right)
+            momentum.y += gravitySpeed / 4 * Time.deltaTime;
+        else if (currentPlayerState == PlayerState.Ground) momentum.y = 0f;
+
+        // INPUTS
+        if (Input.GetKeyDown(KeyCode.LeftShift)) moveSpeed = 100f;
+        if (Input.GetKeyUp(KeyCode.LeftShift)) moveSpeed = 10f;
+        if (Input.GetAxis("Jump") > 0 && currentPlayerState != PlayerState.Jumping)
+        {
+            momentum.y += jumpSpeed;
+            if (currentPlayerState == PlayerState.Wall_Left) momentum.x = wallLaunchSpeed;
+            else if (currentPlayerState == PlayerState.Wall_Right) momentum.x = -wallLaunchSpeed;
+            currentPlayerState = PlayerState.Jumping;
+        }
+        if (momentum.y > jumpSpeed) momentum.y = jumpSpeed;
+        if (momentum.x > 0) momentum.x += momentumDamp * Time.deltaTime;
+        else if (momentum.x < 0) momentum.x -= momentumDamp * Time.deltaTime;
+        if (momentum.z > 0) momentum.z += momentumDamp * Time.deltaTime;
+        else momentum.z -= momentumDamp * Time.deltaTime;
+
+        velocity += momentum;
         velocity *= Time.deltaTime;
         controller.Move(velocity);
+        //Debug.Log(currentPlayerState);
+        if (Input.GetKeyDown(KeyCode.E)) transform.rotation = Quaternion.identity;
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Camera.main.GetComponent<Camera_Titanfall>().camSpeed * Time.deltaTime, Space.World);
+    }
+
+    void FixedUpdate()
+    {
+        if (!CheckForWall())
+        {
+            if (controller.isGrounded) currentPlayerState = PlayerState.Ground;
+            else currentPlayerState = PlayerState.Jumping;
+        }
+    }
+
+    bool CheckForWall()
+    {   
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.red);
+            currentPlayerState = PlayerState.Wall_Right;
+            return true;
+        }
+        else Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right), Color.green);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.red);
+            currentPlayerState = PlayerState.Wall_Left;
+            return true;
+        }
+        else Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left), Color.green);
+        return false;
     }
 }
